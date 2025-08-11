81e786db6d5142c494c5273f2e8eca0387f311d4
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2025-08-10 23:38:12 -0300

    Prepend double dash to option names

--- pykickstart/commands/harddrive.py
+++ pykickstart/commands/harddrive.py
@@ -87,7 +87,7 @@ class FC3_HardDrive(KickstartCommand):
 
         if self.biospart is None and self.partition is None or \
            self.biospart is not None and self.partition is not None:
-            raise KickstartParseError(_("One of biospart or partition options must be specified."), lineno=self.lineno)
+            raise KickstartParseError(_("One of --biospart or --partition options must be specified."), lineno=self.lineno)
 
         return self
 
